3074d478aeb88eedf83521364bbf0b760bc984dc
Nemanja Markovic <necamark@gmail.com>
2024-04-23 04:15:08 +0200

    day 16; intro to OOP

---
+++ main.py
@@ -0,0 +1,20 @@
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+should_ask = True
+while should_ask:
+    choice = input("What would you like? (" + menu.get_items() + "): ")
+    if choice == "off":
+        should_ask = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
